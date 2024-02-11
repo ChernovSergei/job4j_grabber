39c2faa2eb02b1179553b0c9fbcbb6c220a7b9de
Sergei Chernov <goldengrail@gmail.com>
2024-02-11 16:24:59 +0600

    2.1. Преобразование даты [#289476 #480110]

--- src/main/java/utils/HabrCareerDateTimeParser.java
+++ src/main/java/utils/HabrCareerDateTimeParser.java
@@ -7,9 +7,6 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        OffsetDateTime offsetDateTime = OffsetDateTime.parse(parse);
-        LocalDateTime localDateTime = LocalDateTime.parse(parse, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        System.out.println(localDateTime.atOffset(offsetDateTime.getOffset()));
-        return  localDateTime;
+        return LocalDateTime.parse(parse, DateTimeFormatter.ISO_DATE_TIME);
     }
 }
